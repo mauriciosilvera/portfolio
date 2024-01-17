806a11f3e90ec7d35648eef5e6a3d00e7a7f89cf
mauriciosilvera <mauricioagustin_@outlook.com>
2024-01-16 23:18:41 -0300

    Changed main font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,19 @@ import ProfileCard from "@/components/ProfileCard";
 import ThemeProvider from "./theme-provider";
 import { Metadata } from "next";
 import { ReactNode } from "react";
+import { DM_Sans } from "next/font/google";
 
 interface Props {
   children: ReactNode;
 }
 
+const dm_sans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
+
 function RootLayout({ children }: Props) {
   return (
     <ThemeProvider>
       <html lang="en" className="flex justify-center">
-        <body className="w-screen h-screen text-gray-800 dark:text-gray-50 overflow-x-hidden flex items-center flex-col bg-background dark:bg-violet lg:flex-row lg:justify-center xl:w-10/12">
-          <section className="flex flex-col items-center w-11/12 lg:w-2/5 lg:h-[85%] lg:justify-start">
+        <body className={`${dm_sans.className} w-screen h-screen text-gray-800 dark:text-gray-50 overflow-x-hidden flex items-center flex-col bg-background dark:bg-violet lg:flex-row lg:justify-center xl:w-10/12`}> <section className="flex flex-col items-center w-11/12 lg:w-2/5 lg:h-[85%] lg:justify-start">
             <div className="min-h-[20%] my-5 hidden lg:block" />
             <ProfileCard />
           </section>
